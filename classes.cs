99bdb6fc46fd1e55300f3908dcf8120abe7306fa
RussSmi <russell_smith@hotmail.com>
2022-02-12 19:27:37 +0000

    Initial code upload

---
+++ classes.cs
@@ -0,0 +1,32 @@
+using System;
+using Azure.AI.TextAnalytics;
+
+namespace Serverless.Openhack
+{
+    public class Rating
+    {
+        public string id { get; set; }
+        public string userId { get; set; }
+        public string productId { get; set; }
+        public string locationName { get; set; }
+        public int rating { get; set; }
+        public string userNotes { get; set; }
+        public DateTime timeStamp { get; internal set; }
+    }
+
+        
+
+    public class Product
+    {
+        public string productId { get; set; }
+        public string productName { get; set; }
+        public string productDescription { get; set; }
+    }
+
+    public class User
+    {
+        public string userId { get; set; }
+        public string userName { get; set; }
+        public string fullName { get; set; }
+    }
+}
